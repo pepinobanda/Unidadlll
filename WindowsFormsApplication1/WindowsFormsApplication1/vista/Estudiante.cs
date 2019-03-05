a76f7936b8f50b9b0978881a138225590252c575
LAB02-06 <LAB02-06@DESKTOP-7KA7BRS>
2019-03-05 11:57:53 -0600

    clase estudiante

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/vista/Estudiante.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.vista
+{
+    class Estudiante
+    {
+        private long _numerocontrol;
+        private string _nombre;
+        private byte _edad;
+        private char _genero;
+
+        public long numerocontrol;
+        public string nombre;
+        public int edad;
+        public char genero;
+
+        public long NumeroControl
+        {
+            get
+            {
+                return this._numerocontrol;
+            }
+            set
+            {
+                if (value>0)
+                {
+                    this._numerocontrol = value;
+                }
+            }
+        }
+    }
+}
